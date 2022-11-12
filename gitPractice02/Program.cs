3fd3bf1f0f12d3966906b80bda747c8025f763ca
Alex <lipenkov.a61@gmail.com>
2022-11-12 16:32:31 +0500

    commit01

---
+++ gitPractice02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace gitPractice;
+
+public class task2
+{
+    public static void Task2()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        DoTask2(a, b, c);
+    }
+
+    private static void DoTask2(int a, int b, int c)
+    {
+        for (int i = a; i < b; i++)
+        {
+            if (i % c == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
